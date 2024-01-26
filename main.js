64da978fa4d40999c20e2e62111f8211f0ae43dc
xodnd0507 <24457545yong@gmail.com>
2024-01-26 22:13:30 +0900

    Test : js update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
     // css에 미리 설정해둔 noneBox의 스타일을 적용함
-    $('#btn').on('click', function(){
+    $('.ham').on('click', function(){
         $('nav').addClass('noneBox');
     })
     // 아이디가 클로즈인 아이를 클릭하면 noneBox를 제거
